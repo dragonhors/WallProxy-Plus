78bf7e4e2f864724d5fe2ac886a2d437121a3e55
ma xinhua <14590509@qq.com>
2019-06-24 16:19:26 +0800

    凡人修仙

---
+++ books/biquge/FanRen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return FanRen
+
+
+class FanRen(xxbiqugebase):
+    title = u'[小说]凡人修仙传仙界篇'
+    description = u'妄语-凡人修仙传之仙界篇'
+    feeds = 'https://www.xbiquge6.com/1_1203/'
